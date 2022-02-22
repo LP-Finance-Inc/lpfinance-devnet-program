6018ae9cbc33c963f72528c8474bfd5288ae57c6
wangliyang <w.liyang@yandex.com>
2022-02-22 08:19:33 +0800

    deposit collaterals and request borrow amount

--- programs/oracle/programs/oracle/src/lib.rs
+++ programs/oracle/programs/oracle/src/lib.rs
@@ -7,6 +7,15 @@ use tokens::{self, token_mint_to, token_burn, token_transfer, TokenMintTo };
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
+// Mainnet-beta data
+const BTC_MINT:Pubkey = new Pubkey('9n4nbM75f5Ui33ZbPYXn59EwSgE8CGsHtAeTH5YFeJ9E');
+const USDC_MINT:Pubkey = new Pubkey('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v');
+const LpBTC_MINT:Pubkey = new Pubkey('9n4nbM75f5Ui33ZbPYXn59EwSgE8CGsHtAeTH5YFeJ9E');
+const LpUSD_MINT:Pubkey = new Pubkey('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v');
+
+const LTV:u64 = 85;
+const DOMINATOR:u64 = 100;
+
 #[program]
 pub mod oracle {
     use super::*;
@@ -14,95 +23,314 @@ pub mod oracle {
         Ok(())
     }
 
-    // Mint lpbtc stable coin
-    // BTC/USD
-    pub fn mint_lpbtc (
-        ctx: Context<MintLpBtc>,
-        btc_amount: u64
-    ) -> ProgramResult {
-        let user_lpbtc = ctx.accounts.user_lpbtc.to_account_info();
-
-        // Get BTC/USD price from pyth network using their client
-        let pyth_price_info = &ctx.accounts.pyth_account;
-        let pyth_price_data = &pyth_price_info.try_borrow_data()?;
-        let pyth_price = pyth_client::cast::<pyth_client::Price>(pyth_price_data);
-
-        let btc_usd_price = pyth_price.agg.price as u64;
-
-        if btc_usd_price < 0 {
-            msg!("Invalid BTC price");
-        }
-
-        // Amount of our CBS(LpBTC) amount
-        let amount_to_mint = btc_usd_price * btc_amount;
-
-        // Mint LpToken using CPI
-        let cpi_program = ctx.accounts.tokens_program.to_account_info();
-        let cpi_accounts = TokenMintTo {
-        }
-        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts;)
-        tokens::cpi::token_mint_to(cpi_context, amount_to_mint);
-        Ok(())
-    }
-
-    // Mint lpusd stable coin
-    // USD/USD
-    pub fn mint_lpusd (
-        ctx: Context<MintLpUSD>,
-        usd_amount: u64
-    ) -> ProgramResult {
-        // Get USD/USD price from pyth network using their client
-        let pyth_price_info = &ctx.accounts.pyth_account;
-        let pyth_price_data = &pyth_price_info.try_borrow_data()?;
-        let pyth_price = pyth_client::cast::<pyth_client::Price>(pyth_price_data);
-
-        let usdc_usd_price = pyth_price.agg.price as u64;
-
-        if usdc_usd_price < 0 {
-            msg!("Invalid USD price");
-        }
-
-        // Amount of our CBS(LpUSD) amount
-        let amount_to_mint = usdc_usd_price * usd_amount;
-
-        // Mint LpToken using CPI
-        let cpi_program = ctx.accounts.tokens_program.to_account_info();
-        let cpi_accounts = TokenMintTo {
-        }
-        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
-        tokens::cpi::token_mint_to(cpi_context, amount_to_mint);
+    pub fn deposit_btc(
+        ctx: Context<DepositCollateral>,
+        amount: u64
+    )-> ProgramResult {
+        msg!("Deposit BTC");
         
+        if ctx.accounts.user_collateral.amount < amount {
+            return Err(ErrorCode::InsufficientAmount.into());
+        }
+
+        let cpi_accounts = Transfer {
+            from: ctx.accounts.collateral_pool.to_account_info(),
+            to: ctx.accounts.user_collateral.to_account_info(),
+            authority: ctx.accounts.user_authority.to_account_info()
+        };
+
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        token::transfer(cpi_ctx, amount);
+
+        let user_account =&mut ctx.accounts.user_account;
+
+        if ctx.accounts.user_collateral.mint == BTC_MINT {
+            user_account.btc_amount = user_account.btc_amount + amount;
+        }
+
+        if ctx.accounts.user_collateral.mint == USDC_MINT {
+            user_account.usdc_amount = user_account.usdc_amount + amount;
+        }
+
+        if ctx.accounts.user_collateral.mint == LpUSD_MINT {
+            user_account.lpusd_amount = user_account.lpusd_amount + amount;
+        }
+
+        if ctx.accounts.user_collateral.mint == LpBTC_MINT {
+            user_account.lpbtc_amount = user_account.lpbtc_amount + amount;
+        }
+
         Ok(())
     }
 
-    // Mint lpcad stable coin
-    // CAD/USD
-    pub fn mint_lpcad (
-        ctx: Context<MintLpCAD>,
-        cad_amount: u64
+    pub fn deposit_sol(
+        ctx: Context<DepositSOL>,
+        amount: u64
     ) -> ProgramResult {
-        // Get USD/USD price from pyth network using their client
-        let pyth_price_info = &ctx.accounts.pyth_account;
+        msg!("Deposit SOL");
+
+        if **ctx.accounts.user_sol.lamports.borrow() < amount {
+            msg!("Insufficient SOL");
+            return;
+        }
+
+        invoke(
+            &system_instruction::transfer(
+                ctx.accounts.user_sol.key,
+                ctx.accounts.sol_pool.key,
+                amount
+            ),
+            &[
+                ctx.accounts.user_sol.to_account_info().clone(),
+                ctx.accounts.sol_pool.clone(),
+                ctx.accounts.system_program.clone()
+            ]
+        )?;
+
+        let user_account = &mut ctx.accounts.user_account;
+        user_account.sol_amount = user_account.sol_amount + amount;
+
+        Ok(())
+    }
+
+    pub fn borrow_lpusd(
+        ctx: Context<BorrowLpUSD>,
+        amount: u64
+    ) -> ProgramResult {
+        msg!("Borrow LpUSD");
+
+        // Borrowable TotalPrice. Need to be calculated with LTV
+        let &mut total_price = 0;
+        let user_account = &mut ctx.accounts.user_account;
+
+        // BTC price
+        let pyth_price_info = &ctx.accounts.pyth_btc_account;
         let pyth_price_data = &pyth_price_info.try_borrow_data()?;
         let pyth_price = pyth_client::cast::<pyth_client::Price>(pyth_price_data);
 
-        let cad_usd_price = pyth_price.agg.price as u64;
+        let btc_price = pyth_price.agg.price as u64;
 
-        if cad_usd_price < 0 {
-            msg!("Invalid CAD price");
+        total_price += btc_price * user_account.btc_amount * LTV / DOMINATOR;
+
+        // SOL price
+        let pyth_price_info = &ctx.accounts.pyth_sol_account;
+        let pyth_price_data = &pyth_price_info.try_borrow_data()?;
+        let pyth_price = pyth_client::cast::<pyth_client::Price>(pyth_price_data);
+
+        let sol_price = pyth_price.agg.price as u64;
+
+        total_price += sol_price * user_account.sol_amount * LTV / DOMINATOR;
+
+        // USDC price
+        let pyth_price_info = &ctx.accounts.pyth_usdc_account;
+        let pyth_price_data = &pyth_price_info.try_borrow_data()?;
+        let pyth_price = pyth_client::cast::<pyth_client::Price>(pyth_price_data);
+
+        let usdc_price = pyth_price.agg.price as u64;
+        // We think LTV as 100% because of same kind of assets
+        total_price += usdc_price * user_account.usdc_amount;
+
+        // LpUSD price
+        // We think LTV as 100% because of same kind of assets
+        let lpusd_price = usdc_price;        
+        total_price += lpusd_price * user_account.lpusd_amount;
+
+        // LpBTC price
+        let lpbtc_price = btc_price;
+        total_price += lpbtc_price * user_account.lpbtc_amount * LTV / DOMINATOR;
+
+        let &mut borrow_value = lpusd_price * amount;
+        if total_price > borrow_value {
+            // Mint
+            let cpi_program = ctx.accounts.tokens_program.to_account_info();
+            let cpi_accounts = TokenMintTo {
+
+            }
+            let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+            tokens::cpi::token_mint_to(cpi_context, amount);
+
+            // LpUSD
+            if user_account.lpusd_amount > 0 && borrow_value  > 0 {
+                if lpusd_price * user_account.lpusd_amount >= borrow_value {
+                    user_account.lpusd_amount = user_account.lpusd_amount - borrow_value / lpusd_price;
+
+                    borrow_value = 0;
+                } else {
+                    borrow_value = borrow_value - lpusd_price * user_account.lpusd_amount;
+                    user_account.lpusd_amount = 0;
+                }
+                // Burn with repayment
+            }
+
+            // LpBTC
+            if user_account.lpbtc_amount > 0 && borrow_value  > 0 {
+                if lpbtc_price * user_account.lpbtc_amount >= borrow_value * DOMINATOR / LTV {
+                    user_account.lpbtc_amount = user_account.lpbtc_amount - borrow_value / lpbtc_price  * DOMINATOR / LTV;
+
+                    borrow_value = 0;
+                } else {
+                    borrow_value = borrow_value - lpbtc_price * user_account.lpbtc_amount * LTV / DOMINATOR;
+                    user_account.lpbtc_amount = 0;
+                }
+            }
+
+            // UDSC
+            if user_account.usdc_amount > 0 && borrow_value  > 0 {
+                if usdc_price * user_account.usdc_amount >= borrow_value {
+                    user_account.usdc_amount = user_account.usdc_amount - borrow_value / usdc_price;
+
+                    borrow_value = 0;
+                } else {
+                    borrow_value = borrow_value - usdc_price * user_account.usdc_amount;
+                    user_account.usdc_amount = 0;
+                }
+            }
+
+            // SOL
+            if user_account.sol_amount > 0 && borrow_value  > 0 {
+                if sol_price * user_account.sol_amount >= borrow_value * DOMINATOR / LTV {
+                    user_account.sol_amount = user_account.sol_amount - borrow_value / sol_price * DOMINATOR / LTV;
+
+                    borrow_value = 0;
+                } else {
+                    borrow_value = borrow_value - sol_price * user_account.sol_amount* LTV / DOMINATOR;
+                    user_account.sol_amount = 0;
+                }
+            }
+
+            // BTC
+            if user_account.btc_amount > 0 && borrow_value  > 0 {
+                if btc_price * user_account.btc_amount >= borrow_value * DOMINATOR / LTV {
+                    user_account.btc_amount = user_account.btc_amount - borrow_value / btc_price * DOMINATOR / LTV;
+
+                    borrow_value = 0;
+                } else {
+                    borrow_value = borrow_value - btc_price * user_account.btc_amount;
+                    user_account.btc_amount = 0;
+                }
+            }
         }
+    }
 
-        // Amount of our CBS(LpCAD) amount
-        let amount_to_mint = cad_usd_price * cad_amount;
+    pub fn borrow_lpbtc(
+        ctx: Context<BorrowLpBTC>,
+        amount: u64
+    ) -> ProgramResult {
+        msg!("Borrow LpBTC");
 
-        // Mint LpToken using CPI
-        let cpi_program = ctx.accounts.tokens_program.to_account_info();
-        let cpi_accounts = TokenMintTo {
-        }
-        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts;)
-        tokens::cpi::token_mint_to(cpi_context, amount_to_mint);
+        // Borrowable TotalPrice. Need to be calculated with LTV
+        let &mut total_price = 0;
+        let user_account = &mut ctx.accounts.user_account;
 
-        Ok(())
+        // BTC price
+        let pyth_price_info = &ctx.accounts.pyth_btc_account;
+        let pyth_price_data = &pyth_price_info.try_borrow_data()?;
+        let pyth_price = pyth_client::cast::<pyth_client::Price>(pyth_price_data);
+
+        let btc_price = pyth_price.agg.price as u64;
+
+        total_price += btc_price * user_account.btc_amount * LTV / DOMINATOR;
+
+        // SOL price
+        let pyth_price_info = &ctx.accounts.pyth_sol_account;
+        let pyth_price_data = &pyth_price_info.try_borrow_data()?;
+        let pyth_price = pyth_client::cast::<pyth_client::Price>(pyth_price_data);
+
+        let sol_price = pyth_price.agg.price as u64;
+
+        total_price += sol_price * user_account.sol_amount * LTV / DOMINATOR;
+
+        // USDC price
+        let pyth_price_info = &ctx.accounts.pyth_usdc_account;
+        let pyth_price_data = &pyth_price_info.try_borrow_data()?;
+        let pyth_price = pyth_client::cast::<pyth_client::Price>(pyth_price_data);
+
+        let usdc_price = pyth_price.agg.price as u64;
+        // We think LTV as 100% because of same kind of assets
+        total_price += usdc_price * user_account.usdc_amount;
+
+        // LpUSD price
+        // We think LTV as 100% because of same kind of assets
+        let lpusd_price = usdc_price;        
+        total_price += lpusd_price * user_account.lpusd_amount;
+
+        // LpBTC price
+        let lpbtc_price = btc_price;
+        total_price += lpbtc_price * user_account.lpbtc_amount * LTV / DOMINATOR;
+
+        let &mut borrow_value = lpbtc_price * amount;
+
+        if total_price > borrow_value {
+            // Mint
+            let cpi_program = ctx.accounts.tokens_program.to_account_info();
+            let cpi_accounts = TokenMintTo {
+
+            }
+            let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+            tokens::cpi::token_mint_to(cpi_context, amount);
+
+            // LpUSD
+            if user_account.lpusd_amount > 0 && borrow_value  > 0 {
+                if lpusd_price * user_account.lpusd_amount >= borrow_value * DOMINATOR / LTV {
+                    user_account.lpusd_amount = user_account.lpusd_amount - borrow_value / lpusd_price * DOMINATOR / LTV;
+
+                    borrow_value = 0;
+                } else {
+                    borrow_value = borrow_value - lpusd_price * user_account.lpusd_amount * LTV / DOMINATOR;
+                    user_account.lpusd_amount = 0;
+                }
+                // Burn with repayment
+            }
+
+            // LpBTC
+            if user_account.lpbtc_amount > 0 && borrow_value  > 0 {
+                if lpbtc_price * user_account.lpbtc_amount >= borrow_value {
+                    user_account.lpbtc_amount = user_account.lpbtc_amount - borrow_value / lpbtc_price;
+
+                    borrow_value = 0;
+                } else {
+                    borrow_value = borrow_value - lpbtc_price * user_account.lpbtc_amount;
+                    user_account.lpbtc_amount = 0;
+                }
+            }
+
+            // UDSC
+            if user_account.usdc_amount > 0 && borrow_value  > 0 {
+                if usdc_price * user_account.usdc_amount >= borrow_value * DOMINATOR / LTV {
+                    user_account.usdc_amount = user_account.usdc_amount - borrow_value / usdc_price * DOMINATOR / LTV;
+
+                    borrow_value = 0;
+                } else {
+                    borrow_value = borrow_value - usdc_price * user_account.usdc_amount * LTV / DOMINATOR;
+                    user_account.usdc_amount = 0;
+                }
+            }
+
+            // SOL
+            if user_account.sol_amount > 0 && borrow_value  > 0 {
+                if sol_price * user_account.sol_amount >= borrow_value * DOMINATOR / LTV {
+                    user_account.sol_amount = user_account.sol_amount - borrow_value / sol_price * DOMINATOR / LTV;
+
+                    borrow_value = 0;
+                } else {
+                    borrow_value = borrow_value - sol_price * user_account.sol_amount * LTV / DOMINATOR;
+                    user_account.sol_amount = 0;
+                }
+            }
+
+            // BTC
+            if user_account.btc_amount > 0 && borrow_value  > 0 {
+                if btc_price * user_account.btc_amount >= borrow_value {
+                    user_account.btc_amount = user_account.btc_amount - borrow_value / btc_price;
+
+                    borrow_value = 0;
+                } else {
+                    borrow_value = borrow_value - btc_price * user_account.btc_amount;
+                    user_account.btc_amount = 0;
+                }
+            }
+        }        
     }
 }
 
@@ -120,4 +348,10 @@ pub struct MintLpBtc<'info> {
 pub struct OracleBumps{
     pub lptoken_account: u8,
     pub lptoken_mint: u8
+}
+
+#[error]
+pub enum ErrorCode {
+    #[msg("Insufficient Amount")]
+    InsufficientAmount
 }
