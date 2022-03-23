75d3ffb671cdd8a6bcf7e21e932ef24cc79c3317
monkeyMax-SVG <monkeymax@yeah.net>
2022-03-23 18:54:10 +0800

    update dapp for whitelist

--- app/src/components/AddWallet/index.js
+++ app/src/components/AddWallet/index.js
@@ -3,9 +3,8 @@ import * as anchor from '@project-serum/anchor';
 import { useWallet } from '@solana/wallet-adapter-react';
 import idl from '../../idls/lpfinance_accounts.json';
 import { ADD_WALLET_Constants, COMMON_Contants } from '../../constants';
-import { readWalletStateAccount } from '../../helpers';
 
-const { PublicKey, Connection, SystemProgram, SYSVAR_RENT_PUBKEY } = anchor.web3;
+const { PublicKey, Connection } = anchor.web3;
 
 const { NETWORK} = COMMON_Contants;
 const { whiteListKey, configAccountKey } = ADD_WALLET_Constants;
@@ -13,35 +12,7 @@ const { whiteListKey, configAccountKey } = ADD_WALLET_Constants;
 export const AddWallet = () => {
     const wallet = useWallet();
     const { publicKey } = wallet;
-<<<<<<< HEAD
-=======
-
->>>>>>> f20ecd0783eac8d050980ae30397029e9aa413ec
     const [accountKey, setAccountKey] = useState('');
-
-    useEffect(async () => {
-        if (!publicKey) {
-            return;
-        }
-        await getInfo();
-        // eslint-disable-next-line 
-    }, [publicKey])
-
-    const getInfo = async () => {
-        try {            
-            // Get info from user's state account
-            const provider = await getProvider();
-
-            // Get info from cbs program's state account
-            const programData = await readWalletStateAccount(provider, stateAccount);
-            console.log("Wallet Program Data:", programData);
-            console.log("Wallet List:", programData.accountList);
-        } catch (err) {
-            console.log(err);
-        }
-
-        // ((LEPP - 100) / 100 ) ^ 365 = APY
-    }
 
     // GET provider
     const getProvider = async () => {
@@ -71,6 +42,8 @@ export const AddWallet = () => {
     const getInfo = async() => {
         try {
 
+            const provider = await getProvider();
+            anchor.setProvider(provider);
             // address of deployed program
             const programId = new PublicKey(idl.metadata.address);    
             // Generate the program client from IDL.
@@ -78,7 +51,8 @@ export const AddWallet = () => {
 
             const whiteListData = await program.account.whiteList.fetch(whiteListKey);
             const configData = await program.account.config.fetch(configAccountKey);
-            const counter = configData.counter.toNumber();
+            console.log(configData, whiteListData)
+            const counter = configData.counter;
             for (let i = 0; i < counter; i++) {
                 console.log("Account List: ", whiteListData.addresses[i].toBase58())
             }
@@ -105,15 +79,8 @@ export const AddWallet = () => {
             await program.rpc.addWhitelistAddresses(addys, {
                 accounts: {
                     authority,
-<<<<<<< HEAD
-                    cbsAccount,
-                    stateAccount,
-                    systemProgram: SystemProgram.programId,
-                    rent: SYSVAR_RENT_PUBKEY
-=======
-                    config,
-                    stateAccount
->>>>>>> f20ecd0783eac8d050980ae30397029e9aa413ec
+                    config: configAccountKey,
+                    whitelist: whiteListKey
                 }
             })
 
